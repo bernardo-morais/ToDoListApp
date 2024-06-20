5118cb4b7540de9aeff5f32ce667c19a6c8a618f
Bernardo Morais <bernardojmorais@gmail.com>
2024-06-20 15:19:49 +0100

    ToDoList App

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.example.todolist.ui.theme.ToDoListTheme
 
@@ -19,10 +16,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ToDoListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
 
-                }
                 TodoListPage(todoViewModel)
 
             }
